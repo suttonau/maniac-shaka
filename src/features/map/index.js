7a209942dfd65592a48354c38e757a753ac18e11
Sean Sutton <sean@thinknimble.com>
2019-09-13 16:54:07 -0400

    Map grid and tiles configured and proportionate

--- src/features/map/index.js
+++ src/features/map/index.js
@@ -1,4 +1,42 @@
 import React from 'react'
+import { SPRITE_SIZE } from '../../config/constants'
+import './styles.css'
+
+function getTileSprite (type) {
+  switch (type) {
+    case 0:
+      return 'grass'
+    case 5:
+      return 'shrub'
+    case 6:
+      return 'tree'
+  }
+}
+
+function MapTile (props) {
+  return (
+    <div
+      className={`tile ${getTileSprite(props.tile)}`}
+      style={{
+        height: SPRITE_SIZE,
+        width: SPRITE_SIZE
+      }}
+    >
+      {props.tile}
+    </div>
+
+  )
+}
+
+function MapRow (props) {
+  return (
+    <div className="row">
+      {
+        props.tiles.map(tile => <MapTile tile={tile} />)
+      }
+    </div>
+  )
+}
 
 function Map (props) {
   return (
@@ -12,7 +50,11 @@ function Map (props) {
         backgroundColor: 'gray',
         border: '4px solid white'
       }}
-    />
+    >
+      {
+        props.tiles.map(row => <MapRow tiles={row} />)
+      }
+    </div>
   )
 }
 
