fc188ec9fdedb441f6d3bda5e20c2e99166ce916
Sean Sutton <sean@thinknimble.com>
2019-09-13 17:36:47 -0400

    implemented boundaries and impassables for each attempted move

--- src/features/player/movement.js
+++ src/features/player/movement.js
@@ -2,8 +2,8 @@ import store from '../../config/store'
 import { SPRITE_SIZE, MAP_WIDTH, MAP_HEIGHT } from '../../config/constants'
 
 export default function handleMovement (player) {
-  function getNewPoisition (direction) {
-    const oldPos = store.getState().player.position
+  function getNewPoisition (oldPos, direction) {
+    // const oldPos = store.getState().player.position
     switch (direction) {
       case 'WEST':
         return [oldPos[0] - SPRITE_SIZE, oldPos[1]]
@@ -16,39 +16,54 @@ export default function handleMovement (player) {
     }
   }
 
+  // return predicate t/f if position is within the map size
   function observeBoundaries (oldPos, newPos) {
     return (newPos[0] >= 0 && newPos[0] <= MAP_WIDTH - SPRITE_SIZE) &&
     (newPos[1] >= 0 && newPos[1] <= MAP_HEIGHT - SPRITE_SIZE)
-      ? newPos : oldPos
   }
 
   function observeImpassable (oldPos, newPos) {
+    const tiles = store.getState().map.tiles
+    const y = newPos[1] / SPRITE_SIZE
+    const x = newPos[0] / SPRITE_SIZE
 
+    const nextTile = tiles[y][x]
+    return nextTile < 5
   }
 
-  function dispatchMove (direction) {
-    const oldPos = store.getState().player.position
+  function dispatchMove (newPos) {
+    // const oldPos = store.getState().player.position
+    // const newPos = getNewPoisition(direction)
     store.dispatch({
       type: 'MOVE_PLAYER',
       payload: {
-        position: observeBoundaries(oldPos, getNewPoisition(direction))
+        position: newPos
       }
     })
+  }
+
+  function attemptMove (direction) {
+    const oldPos = store.getState().player.position
+    const newPos = getNewPoisition(oldPos, direction)
+
+    if (observeBoundaries(oldPos, newPos) && observeImpassable(oldPos, newPos)) {
+      dispatchMove(newPos)
+    }
   }
 
   function handleKeyDown (e) {
     switch (e.keyCode) {
       case 37:
-        return dispatchMove('WEST')
+        return attemptMove('WEST')
 
       case 38:
-        return dispatchMove('NORTH')
+        return attemptMove('NORTH')
 
       case 39:
-        return dispatchMove('EAST')
+        return attemptMove('EAST')
 
       case 40:
-        return dispatchMove('SOUTH')
+        return attemptMove('SOUTH')
       default:
         console.log(e.keyCode)
     }
